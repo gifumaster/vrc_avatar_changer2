b51a64a286d3a7597562b37280b4a14d89f08a89
gifumaster <gifumaster@gmail.com>
2024-08-09 16:58:20 +0900

    修正

--- vrc_avatar_change2/src/main.ts
+++ vrc_avatar_change2/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify.js";
+// @ts-ignore
+import vuetify from "./plugins/vuetify.js"; 
 
 createApp(App).use(vuetify).mount("#app");
