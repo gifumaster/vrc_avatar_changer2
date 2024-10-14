e3429ce1bca272eb0c37047567165d431898c4d8
gifumaster <gifumaster@gmail.com>
2024-10-14 19:18:15 +0900

    画像が外部から取れなくなったのでそのあたりを一時的にオミット

--- src/components/AvatarChangeDialog.vue
+++ src/components/AvatarChangeDialog.vue
@@ -5,30 +5,12 @@
         <v-card-title class="text-h5 grey lighten-2"> </v-card-title>
 
         <v-card-text>
-          <div style="width: 450px; height: 333px">
-            <v-img
-              max-width="450px"
-              max-height="333px"
-              aspect-ratio="4/3"
-              cover
-              :src="avatarImage"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-row> </template
-            ></v-img>
-          </div>
           アバター「{{ avatarName }}」に変更しますか？
         </v-card-text>
 
         <v-divider></v-divider>
         <v-card-actions>
           <a :href="url" target="_blank"> VRChat </a>
-          <v-btn color="primary" text @click="updated"> サムネを更新 </v-btn>
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="execute"> 変更する </v-btn>
           <v-btn color="secondary" text @click="cancel"> しない </v-btn>
